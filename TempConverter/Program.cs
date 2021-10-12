5428ad88de67fcf87bd0ef3bcb714693420e0d0f
Caprena Richie <robcaprena@gmail.com>
2021-10-12 17:18:18 -0400

    first commit

---
+++ TempConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var fToc = TempConverter.FahrenheitToCelsius(45);
+           var cTof  = TempConverter.CelsiusToFahrenheit(0);
+            Console.WriteLine(fToc);
+            Console.WriteLine(cTof);
+
+        }
+    }
+}
---
+++ TempConverter/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TempConverter
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+        public static double CelsiusToFahrenheit (double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+    
+    
+    
+
+
+
+}
